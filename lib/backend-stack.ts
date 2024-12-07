17d240023bac9486af91e9ccd419a4a5ac7e8381
Workshop <workshop@serverlesscreed.com>
2024-12-07 16:05:28 +0530

    Final PostConfirm

--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import path = require("path");
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -35,5 +36,23 @@ export class BackendStack extends cdk.Stack {
       tableName: "Orders",
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
+
+    /* Creating our Lambda function for PostConfirmation
+     * - This function is triggered after a user is confirmed
+     * - It creates a user in our (Users) table
+     *  - Grants write access to the Users table
+     */
+    const postConfirmation = new cdk.aws_lambda_nodejs.NodejsFunction(
+      this,
+      "PostConfirmationFunction",
+      {
+        entry: path.join(__dirname, "PostConfirmation", "handler.ts"),
+        handler: "handler",
+        environment: {
+          TABLE_NAME: UsersTable.tableName,
+        },
+      }
+    );
+    UsersTable.grantWriteData(postConfirmation);
   }
 }

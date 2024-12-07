ea25525f049ffd167a00d1c8f0b33530efc10a76
Workshop <workshop@serverlesscreed.com>
2024-12-07 15:41:23 +0530

    Constructing Our DynamoDB Tables

--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -1,16 +1,39 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    /* Creating our DynamoDB table
+     * - Users Table
+     * - Product Table
+     * - Orders Table
+     */
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const UsersTable = new cdk.aws_dynamodb.TableV2(this, "UsersTable", {
+      partitionKey: {
+        name: "UserID",
+        type: cdk.aws_dynamodb.AttributeType.STRING,
+      },
+      tableName: "Users",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    const ProductTable = new cdk.aws_dynamodb.TableV2(this, "ProductTable", {
+      partitionKey: {
+        name: "ProductID",
+        type: cdk.aws_dynamodb.AttributeType.STRING,
+      },
+      tableName: "Products",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    const OrderTable = new cdk.aws_dynamodb.TableV2(this, "OrderTable", {
+      partitionKey: {
+        name: "OrderID",
+        type: cdk.aws_dynamodb.AttributeType.STRING,
+      },
+      tableName: "Orders",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
